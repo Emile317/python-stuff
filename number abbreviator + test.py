5f48d6c6127c5c199762533cffa6ac1419c3ccd7
Emile Muntinga <97318654+Emile317@users.noreply.github.com>
2022-05-11 23:30:11 +0200

    new update
    added terminal interaction
    major bug, for some reason calling input_num() function does not work!!!!!!

--- number abbreviator + test.py
+++ number abbreviator + test.py
@@ -98,10 +98,58 @@ def get_work(mass, acceleration, distance):
 #end of functions
 
 train_force = get_force(train_mass, train_acceleration)
-print('The GE train supplies ' + str(number_letter_maker(train_force)) + ' Newtons of force.')
+#print('The GE train supplies ' + str(number_letter_maker(train_force)) + ' Newtons of force.')
 
 bomb_energy = get_energy(bomb_mass)
-print('A 1kg bomb supplies ' + str(number_letter_maker(bomb_energy)) + ' Joules')
+#print('A 1kg bomb supplies ' + str(number_letter_maker(bomb_energy)) + ' Joules')
 
 train_work = get_work(train_mass, train_acceleration, train_distance)
-print('The GE train does ' + str(number_letter_maker(train_work)) + ' Joules of work over ' + str(number_letter_maker(train_distance)) + ' meters.')
+#print('The GE train does ' + str(number_letter_maker(train_work)) + ' Joules of work over ' + str(number_letter_maker(train_distance)) + ' meters.')
+
+print('''Hello, welcome to Emile's number abbreviator!''')
+print('''In this program you can put in a number and it will round it to two decimals and putting the abbreviation of the number behind it.
+E.g if you put in 19232 the program will output ''' + number_letter_maker(19232) + '.')
+
+def input_num():
+    input_num = input('Put in any number: ')
+    input_num_int = int(input_num)
+    return input_num_int
+
+input_num = input_num()
+
+def answer():
+    if input_num < 1000:
+        print('Your number is too low! Please put in a higher number below.')
+        input_num()
+    elif input_num >= 10**24:
+        print('Your number is too high! Please put in another number below.')
+        input_num()
+    else:
+        print('Here is your abbreviated number: ' + number_letter_maker(input_num))
+        print('''Do you want to abbreviate another number?
+        
+        Please select an option:
+        1: yes
+        2: no
+        ''')
+        second_input_num = input('')
+
+        if second_input_num == '1':
+            input_num()
+            answer()
+        elif second_input_num == '2':
+            input_num()
+            answer()
+        else:
+            second_input_num = input('Please select a valid option: ')
+            if second_input_num == '1':
+                input_num()
+                answer()
+            elif second_input_num == '2':
+                input_num()
+                answer()
+            else:
+                print('fuck off, idk how to make this work how hard is it to put in a number i gave you a second chance even.')
+                quit()
+                
+answer()
